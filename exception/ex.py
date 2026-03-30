409b72f0357e4a69f078f4ff86a1e352ca5a6ff6
unknown <lazaltron@fecea.edu.ar>
2026-03-30 20:57:41 -0300

    cambios

--- exception/ex.py
+++ exception/ex.py
@@ -1,4 +1,15 @@
 try:
-    1/0
+    raise StopIterarion("Parar de correr") #tirar una exception a proposito, con un mensaje para que no tire error el e.args
+    #- Por default las excepciones que tiramos no tienen mensaje
+    "Hola"**2 #TypeError
+    1/0 #ZeroDivError
 except ZeroDivisionError:
     print("No se puede dividir por cero")
+except TypeError:
+    print("Error de tipo")
+except Exception as e:
+    print("Ocurrio un error: {}".format(e.args[0]))
+finally:
+    print("Esto se eejcuta si o si.")
+#Todas las excepciones heredan de la clase Exception
+#StopIterarion es un tipo de iteracion utilizada en el for.
