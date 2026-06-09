4f9dcd48b1705fc9b2325d8892f06bbc6708499c
unknown <lazaltron@fecea.edu.ar>
2026-06-08 21:12:29 -0300

    orm

--- ORM2/main.py
+++ ORM2/main.py
@@ -2,12 +2,14 @@ import sqlobject as SO
 from dotenv import load_dotenv
 import os
 
+#conexion db
 load_dotenv()
 
 database = "mysql://{}:{}@{}/{}".format(os.getenv("USER"), os.getenv("PASSWORD"), os.getenv("SERVER"), os.getenv("DB"))
 
 __connection__ = SO.connectionForURI(database)
 
+#creacion de clases
 class Area(SO.SQLObject):
     nombre = SO.StringCol(length=40, varchar=True)
     empleados = SO.MultipleJoin("Empleado")
@@ -18,14 +20,31 @@ class Empleado(SO.SQLObject):
     hijos = SO.IntCol()
     activo = SO.BoolCol()
     area = SO.ForeignKey("Area",default=None,cascade=False) #Cascade modifica a todo lo que interpele este empleado
+    habilidades = SO.RelatedJoin("Habilidad")
+
+class Habilidad(SO.SQLObject):
+    nombre = SO.StringCol(length=40, varchar=True)
+    peso =  SO.FloatCol()
+    empleados = SO.RelatedJoin("Empleado")
+
 
 #borro las tablas para ir testeando
+Habilidad.dropTable(ifExists = True)
 Empleado.dropTable(ifExists = True)
 Area.dropTable(ifExists = True)
 
 #creacion de tablas
 Area.createTable()
 Empleado.createTable()
+Habilidad.createTable() 
+# Por ser una relacion muchos a muchos, se crea automaticamente en la bd empleado_habilidad
+# tambien crea automaticamente dos metodos a la clase empleado (addHabilidad, removeHabilidad)
+# y otros dos para la clase Habilidad (addEmpleado, removeEmpleado)
+
+#creacion de habilidades
+calculo = Habilidad(nombre="CALCULO", peso = 8.9)
+programacion = Habilidad(nombre="PROGRAMACION", peso = 9.5)
+relacionesInterp = Habilidad(nombre="RELACIONES INTERPERSONALES", peso = 6.2)
 
 #creacion de areas
 contabilidad = Area(nombre="CONTABILIDAD")
@@ -55,3 +74,10 @@ for emp in areaRRHH.getOne().empleados:
 #como saber en q area trabaja un empleado
 emp = Empleado.get(1)
 print(emp.area.nombre)
+
+#agregar empleados a habilidades
+programacion.addEmpleado(Empleado.get(1))
+relacionesInterp.addEmpleado(Empleado.get(2))
+
+#agregar habilidad a un empleado
+Empleado.get(3).addHabilidad(Habilidad.get(1))
